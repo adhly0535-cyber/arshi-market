e62f42eaa39c8d7532087ec7f308b7fd64ed9ef6
adhly0535-cyber <adhly0535@gmail.com>
2026-01-18 13:23:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// اختبار أن الجافاسكربت شغال
+alert("JavaScript شغال ✅");
+
+// Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
+// إعدادات Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyD3hAvhSDs-1X7ORCORODT31_s_pJAz8",
   authDomain: "arshi-market.firebaseapp.com",
@@ -10,18 +15,20 @@ const firebaseConfig = {
   appId: "1:930764899868:web:06795817be840a8a01d9"
 };
 
+// تشغيل Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
+// زر تسجيل الدخول
 const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", () => {
   signInWithPopup(auth, provider)
     .then((result) => {
-      alert("تم تسجيل الدخول بنجاح: " + result.user.displayName);
+      alert("تم تسجيل الدخول بنجاح ✅\n" + result.user.displayName);
     })
     .catch((error) => {
-      alert("خطأ: " + error.message);
+      alert("خطأ ❌\n" + error.message);
     });
 });
