9e9a4ec7e464993208417a1960573fe086e69bcc
adhly0535-cyber <adhly0535@gmail.com>
2026-01-18 13:13:50 +0200

    add google login

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Arshi Market is running");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3hAvhSDs-1X7ORCORODT31_s_pJAz8",
+  authDomain: "arshi-market.firebaseapp.com",
+  projectId: "arshi-market",
+  storageBucket: "arshi-market.appspot.com",
+  messagingSenderId: "930764899868",
+  appId: "1:930764899868:web:06795817be840a8a01d9"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      alert("تم تسجيل الدخول بنجاح: " + result.user.displayName);
+    })
+    .catch((error) => {
+      alert("خطأ: " + error.message);
+    });
+});
